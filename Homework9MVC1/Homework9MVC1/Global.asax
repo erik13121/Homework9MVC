47a31f5043921d2087c2e3355ad726767761ad6a
Erik Grassman <erik13121@yahoo.com>
2018-05-01 16:28:34 +0200

    Added other solution

---
+++ Homework9MVC1/Homework9MVC1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Homework9MVC1.MvcApplication" Language="C#" %>
---
+++ Homework9MVC1/Homework9MVC1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Homework9MVC1.Startup))]
+namespace Homework9MVC1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
